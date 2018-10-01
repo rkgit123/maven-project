a514b814ed7ffed6071d53b3b07044c48a5d278e
rkgit123 <42130065+rkgit123@users.noreply.github.com>
2018-10-01 13:09:59 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public String strack(String strack) {
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
