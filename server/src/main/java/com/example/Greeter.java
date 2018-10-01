365b6939730aca09d4dce8bb0427b8c1c8b20166
rkgit123 <42130065+rkgit123@users.noreply.github.com>
2018-10-01 13:31:38 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
+/**
+*@param someone the name of a person
+*@return greeting string
+*/
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
